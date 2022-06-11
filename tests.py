3594a48df1ea20771c928655c08758643f4f3d26
Steven Hynes <epicstevo555@gmail.com>
2022-06-11 11:02:35 -0400

    added test function for approx eval

--- tests.py
+++ tests.py
@@ -1,9 +1,10 @@
+import time
 from naive import *
 
 def test_add_edge_and_eval():
     cities = cities_from_file('data/us-cities-top-1k.csv')
     complete = complete_solution(cities)
-    greedy_sol = greedy_buildup(cities, 50)
+    greedy_sol = greedy_buildup(cities, 1000)
     prev_apsp, prev_score = evaluate_solution(greedy_sol)
     la_ny = "Los Angeles, California", "New York, New York", complete.edges["Los Angeles, California", "New York, New York"]
     test_graph, test_apsp, test_score = add_edge_and_eval(greedy_sol, la_ny, prev_apsp)
@@ -26,7 +27,7 @@ def test_add_edge_and_eval():
 def test_remove_edge_and_eval():
     cities = cities_from_file('data/us-cities-top-1k.csv')
     complete = complete_solution(cities)
-    greedy_sol = greedy_buildup(cities, 50)
+    greedy_sol = greedy_buildup(cities, 1000)
     prev_apsp, prev_score = evaluate_solution(greedy_sol)
     nj_ny = "Jersey City, New Jersey", "New York, New York", complete.edges["Jersey City, New Jersey", "New York, New York"]
     test_graph, test_apsp, test_score = remove_edge_and_eval(greedy_sol, nj_ny, prev_apsp)
@@ -45,3 +46,15 @@ def test_remove_edge_and_eval():
         except:
             print("%.20f, %.20f" % (test_apsp["New York, New York"][city], bench_apsp["New York, New York"][city]))
     assert bench_score == test_score
+
+def test_approx_evaluate_solution():
+    cities = cities_from_file('data/us-cities-top-1k.csv')
+    # complete = complete_solution(cities)
+    greedy_sol = greedy_buildup(cities, 1000)
+    sorted_nodes = sorted(greedy_sol.nodes, key=lambda x: greedy_sol.nodes.data()[x]['population'], reverse=True)
+
+    # print(evaluate_solution(greedy_sol)[1])
+    t = time.time()
+    print(approx_evaluate_solution(greedy_sol, [], len(greedy_sol)))
+    print(approx_evaluate_solution(greedy_sol, [], 200))
+    print(approx_evaluate_solution(greedy_sol, sorted_nodes[:20], 180))
