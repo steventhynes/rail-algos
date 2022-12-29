7fe89a6cd1e33e12dba038dcf757572b6b062e38
Steven Hynes <epicstevo555@gmail.com>
2022-12-28 19:48:39 -0500

    fixed using empty graph and added more algorithms to test

--- composite.py
+++ composite.py
@@ -1,9 +1,9 @@
 from random import shuffle
-import time
-from util import cities_from_file, display_solution, empty_solution, complete_solution
 
-def naive_merge(cities, k, solutions):
-    complete = complete_solution(cities)
+from numpy import disp
+from util import cities_from_file, display_solution
+
+def naive_merge(empty, complete, k, solutions):
     edge_counts = {}
     for sol in solutions:
         for edge in sol.edges:
@@ -11,7 +11,7 @@ def naive_merge(cities, k, solutions):
     edges = list(edge_counts.keys())
     shuffle(edges)
     sorted_edges = sorted(edges, key=lambda x: edge_counts[x], reverse=True)
-    new_sol = empty_solution(cities)
+    new_sol = empty.copy()
     for edge in sorted_edges:
         if new_sol.size('dist') + complete.edges[edge]['dist'] < k:
             new_sol.add_edge(*edge, dist=complete.edges[edge]['dist'])
@@ -19,14 +19,21 @@ def naive_merge(cities, k, solutions):
 
 def test():
     from naive import greedy_buildup, min_dist_spanning_tree_buildup, max_weight_spanning_tree_buildup
-    from heuristic import ant_colony_optimization
-    cities = cities_from_file('data/us-cities-top-1k.csv')
-    # sol1 = greedy_buildup(cities, 1000)
-    # sol2 = max_weight_spanning_tree_buildup(cities, 1000)
-    # sol3 = ant_colony_optimization(cities, 1000)
-    # final = naive_merge(cities, 1000, [sol1, sol2, sol3])
-    sols = [ant_colony_optimization(cities, 1000) for i in range(6)]
-    final = naive_merge(cities, 1000, sols)
+    from heuristic import ant_colony_optimization, evolutionary_algorithm
+    empty, complete = cities_from_file('data/us-cities-top-1k.csv')
+    sol1 = greedy_buildup(empty, complete, 1000)
+    display_solution(sol1)
+    sol2 = min_dist_spanning_tree_buildup(empty, complete, 1000)
+    display_solution(sol2)
+    sol3 = max_weight_spanning_tree_buildup(empty, complete, 1000)
+    display_solution(sol3)
+    sol4 = ant_colony_optimization(empty, complete, 1000)
+    display_solution(sol4)
+    sol5 = evolutionary_algorithm(empty, complete, 1000)
+    display_solution(sol5)
+    sols = [sol1, sol2, sol3, sol4, sol5]
+    # sols = [ant_colony_optimization(cities, 1000) for i in range(5)]
+    final = naive_merge(empty, complete, 1000, sols)
     display_solution(final)
     return final
 
